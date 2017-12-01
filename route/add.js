832c5699aa067ef195d0247c907bbf5bb1ed155d
aayush1408 <aayushjaiswal0114@gmail.com>
2017-12-01 22:45:01 +0530

    Data added

--- route/add.js
+++ route/add.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/mysql');
 
-router.post('/form',(req,res)=>{
-    res.render('form');
+router.post('/toy',(req,res)=>{
+    let toys = {name:req.body.name,address:req.body.address,phone:req.body.phone,toy_name:req.body.toy_name,description:req.body.description};
+    let sql = `INSERT INTO toy SET ?`;
+    let query = db.query(sql,toys,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send('Toy added');
+    });     
+
 });
-
 module.exports = router;
 
--- route/display.js
+++ route/display.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/toy',(req,res)=>{
-    let toys = {name:req.body.name,body:'Motherfucker'};
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query(sql,post,(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-        res.send('Post added');
-    });     
-
+router.get('/form',(req,res)=>{
+    res.render('form');
 });
 
 module.exports = router;
